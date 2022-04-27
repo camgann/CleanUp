fc2be23f8ea3dd0943d58a0a6466e4c64d7bdb45
cgann439 <campbell.gann@gmail.com>
2022-04-27 13:44:53 -0500

    2nd commit test

--- CleanUpCapstone/app/src/main/java/com/example/cleanup_capstone/InProgressClass.java
+++ CleanUpCapstone/app/src/main/java/com/example/cleanup_capstone/InProgressClass.java
@@ -6,16 +6,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class InProgressClass extends Activity{
     Button backButton;
+    Button button;
+    TextView receiver_msg;
+    TableRow tableRow;
+    TextView textView;
+    TableLayout tableLayout;
+    ArrayList<String> receivedChoice = new ArrayList<String>();
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inprogress);
         addListenerOnButton(R.id.backButton, backButton, ButtonSceneClass.class);
+        tableLayout = (TableLayout) findViewById(R.id.inProgressTableLayout);
+
+        receiver_msg = (TextView)findViewById(R.id.check);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("messageKey");
+        receiver_msg.setText(str);
+        receivedChoice.add((String)receiver_msg.getText());
+        addToTable();
+//        for (int i = 0; i < 4; i++) {
+//            tableRow = new TableRow(getApplicationContext());
+//            for (int j = 0; j < 3; j++) {
+//                textView = new TextView(getApplicationContext());
+//                textView.setText(receiver_msg.getText());
+//                //receiver_msg.setText(str);
+//                textView.setPadding(50, 100, 20, 20);
+//                tableRow.addView(textView);
+//            }
+//            tableLayout.addView(tableRow);
+//        }
+    }
+
+    public void addToTable(){
+        tableRow = new TableRow(getApplicationContext());
+        textView = new TextView(getApplicationContext());
+        textView.setText(receivedChoice.get(receivedChoice.size()-1));
+        textView.setTextSize(38);
+        textView.setPadding(50, 100, 20, 20);
+        tableRow.addView(textView);
+        textView = new TextView(getApplicationContext());
+        textView.setText("Hey");
+        textView.setTextSize(38);
+        tableRow.addView(textView);
+        tableLayout.addView(tableRow);
+
     }
 
     public void addListenerOnButton(int b, Button button1, Class c) {
--- CleanUpCapstone/app/src/main/java/com/example/cleanup_capstone/NewSceneClass.java
+++ CleanUpCapstone/app/src/main/java/com/example/cleanup_capstone/NewSceneClass.java
@@ -5,16 +5,62 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class NewSceneClass extends Activity{
     Button backButton;
+    String choice2 = "begin";
+    ArrayList<String> choice = new ArrayList<String>();
+    Button goButton;
+    Spinner houseItemsSpin;
+    Spinner daysSpin;
+    Spinner weeksSpin;
+    ArrayList<String> houseItems = new ArrayList<String>();
+    Integer[] days = {1,2,3,4,5,6,7};
+    Integer[] weeks = new Integer[20];
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newscene);
         addListenerOnButton(R.id.backButton2, backButton, ButtonSceneClass.class);
+        addListenerOnButton(R.id.goButton, goButton, InProgressClass.class);
+        houseItemsSpin = (Spinner) findViewById(R.id.itemsSpinner);
+        daysSpin = (Spinner) findViewById(R.id.daysSpin);
+        weeksSpin = (Spinner) findViewById(R.id.weeksSpin);
+
+        for(int i= 0; i < weeks.length; i++){
+            weeks[i] = i + 1;
+        }
+        houseItems.add("Kitchen Sink");
+        houseItems.add("Blinds");
+        houseItems.add("Bathroom Sink");
+        houseItems.add("Mop Floor");
+        houseItems.add("Vacuum Main Floor");
+        houseItems.add("Wash bed sheets");
+        houseItems.add("clean whole bathroom");
+        houseItems.add("clean toilet");
+
+        //adding items to arrayAdapter
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, houseItems);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        houseItemsSpin.setAdapter(arrayAdapter);
+
+        ArrayAdapter<Integer> arrayAdapter2 = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, days);
+        arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daysSpin.setAdapter(arrayAdapter2);
+
+        ArrayAdapter<Integer> arrayAdapter3 = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, weeks);
+        arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        weeksSpin.setAdapter(arrayAdapter3);
+
     }
 
     public void addListenerOnButton(int b, Button button1, Class c) {
@@ -26,8 +72,29 @@ public class NewSceneClass extends Activity{
             public void onClick(View view) {
                 Intent intent = new Intent(context, c);
                 startActivity(intent);
+                if(b == R.id.goButton){
+                    String s = "works";
+                    choice.add(houseItemsSpin.getSelectedItem().toString());
+                    Intent sendIntent = new Intent(getApplicationContext(), InProgressClass.class);
+                    sendIntent.putExtra("messageKey", choice.get(choice.size()-1));
+                    startActivity(sendIntent);
+                }
             }
         });
     }
+
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id){
+        Toast.makeText(getApplicationContext(), houseItems.get(position), Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), days[position], Toast.LENGTH_LONG).show();
+    }
+
+    public void onNoSelection(AdapterView<?> arg0){
+
+    }
+
+
+
+
+
 
 }
